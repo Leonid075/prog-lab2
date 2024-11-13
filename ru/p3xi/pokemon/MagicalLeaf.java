7d0a51fa77dd8e7f7f56446d1ea6b8340ca127a3
Leonid075 <grebenukleonid075@gmail.com>
2024-11-13 13:50:20 +0300

    finaling classes

--- ru/p3xi/pokemon/MagicalLeaf.java
+++ ru/p3xi/pokemon/MagicalLeaf.java
@@ -1,7 +1,7 @@
 package ru.p3xi.pokemon;
 import ru.ifmo.se.pokemon.*;
 
-public class final MagicalLeaf extends SpecialMove {
+public final class MagicalLeaf extends SpecialMove {
     public MagicalLeaf() {
         super(Type.GRASS, 60, Double.POSITIVE_INFINITY);
     }
